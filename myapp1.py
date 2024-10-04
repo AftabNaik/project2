78f4a354ea5b4b5624a7b5fbecb5a644605cc2a6
AftabNaik <75939321+AftabNaik@users.noreply.github.com>
2024-10-04 16:40:43 +0530

    Update myapp1.py

--- myapp1.py
+++ myapp1.py
@@ -1,11 +1,11 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask
-
+from flask import Flask 
+#hello boys
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
-
+#welcome
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
