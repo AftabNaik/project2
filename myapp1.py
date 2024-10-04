37e4aeda7b914d72190284dd37a86425e40499bd
AftabNaik <75939321+AftabNaik@users.noreply.github.com>
2024-10-04 16:42:47 +0530

    Update myapp1.py

--- myapp1.py
+++ myapp1.py
@@ -1,7 +1,7 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 from flask import Flask 
-#hello boys
+#hello idiot
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
